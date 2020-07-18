36269c8fac3c01682f0ca3b675b101fd1ca5f328
rikinp <rikin201@gmail.com>
2020-07-18 16:33:28 +0530

    Final commit without alert

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/questions.dart';
+import 'package:quizzler/quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +27,26 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+//  List<String> questions = [
+//    'You can lead a cow down stairs but not up stairs.',
+//    'Approximately one quarter of human bones are in the feet.',
+//    'A slug\'s blood is green.'
+//  ];
+//  List<bool> answers = [
+//    false,
+//    true,
+//    true
+//  ];
+
+//  List<Question> questionBank = [
+//    Question(q:'You can lead a cow down stairs but not up stairs.',a:false),
+//    Question(q:'Approximately one quarter of human bones are in the feet.',a:true),
+//    Question(q:'A slug\'s blood is green.',a:true),
+//  ];
+  QuizBrain quizBrain = QuizBrain();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +59,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +83,25 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  if(quizBrain.getAnswer()==true){
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                    );
+                  }
+                  else{
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        )
+                    );
+                  }
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
@@ -79,12 +119,32 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  if(quizBrain.getAnswer()==false){
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                    );
+                  }
+                  else{
+                    scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        )
+                    );
+                  }
+                  quizBrain.nextQuestion();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
